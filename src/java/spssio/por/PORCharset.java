fa7cac0b5274dfe9d6356f5035fa4eae7a01c742
Jani Hautamaki <jani.hautamaki@hotmail.com>
2013-01-12 18:42:08 +0200

    The basic set up of the project plus some initial source files to test the project build configurations.

---
+++ src/java/spssio/por/PORCharset.java
@@ -0,0 +1,345 @@
+//*******************************{begin:header}******************************//
+//                 spssio - http://code.google.com/p/spssio/                 //
+//***************************************************************************//
+//
+//      Java classes for reading and writing 
+//      SPSS/PSPP Portable and System files
+//
+//      Copyright (C) 2013 Jani Hautamaki <jani.hautamaki@hotmail.com>
+//
+//      Licensed under the terms of GNU General Public License v3.
+//
+//      You should have received a copy of the GNU General Public License v3
+//      along with this program as the file LICENSE.txt; if not, please see
+//      http://www.gnu.org/licenses/gpl-3.0.html
+//
+//********************************{end:header}*******************************//
+
+package spssio.por;
+
+
+/**
+ * Character translation tables for portable files.<p>
+ *
+ * According to PSPP's documentation
+ * <a href="http://www.gnu.org/software/pspp/pspp-dev/html_node/Portable-File-Characters.html#Portable-File-Characters">
+ *   A.1 Portable File Characters
+ * </a> 
+ * and
+ * <a href="http://www.gnu.org/software/pspp/pspp-dev/html_node/Portable-File-Header.html#Portable-File-Header">
+ *   A.3 Portable File Header
+ * </a>:<p>
+ * 
+ * "The file contents may be in any character set; the file contains 
+ *  a description of its own character set [...] Therefore, the `Z' character 
+ * is not necessarily an ASCII `Z'."<p>
+ *
+ * "Symbols that are not defined in a particular character set are set to 
+ * the same value as symbol 64; i.e., to `0'."<p>
+ *
+ * The constant array {@link #TRANS} contains the following table which has 
+ * been pulled out from the PSPP's documentation:<p>
+ *
+ * <code><pre>
+ *  0-60     Control characters. Not important enough to describe in full here.
+ *  61-63    Reserved
+ *  64-73    Digits `0` through `9`
+ *  74-99    Capital letters `A` through `Z`
+ *  100-125  Lowercase letters `a` through `z`
+ *  126      Space
+ *  127-130  Symbols .<(+
+ *  131      Solid vertical pipe
+ *  132-142  Symbols &[]!$*);^-/
+ *  143      Broken vertical pipe
+ *  144-150  Symbols ,%_>?`:
+ *  151      British pound symbol
+ *  152-155  Symbols @'="
+ *  156      Less than or equal symbol
+ *  157      Empty box
+ *  158      Plus or minus
+ *  159      Filled box
+ *  160      Degree symbol
+ *  161      Dagger
+ *  162      Symbol `~`
+ *  163      En dash
+ *  164      Lower left corner box draw
+ *  165      Upper left corner box draw
+ *  166      Greater than or equal symbol
+ *  167-176  Superscript `0` through `9`
+ *  177      Lower right corner box draw
+ *  178      Upper right corner box draw
+ *  179      Not equal symbol
+ *  180      Em dash
+ *  181      Superscript `(`
+ *  182      Superscript `)`
+ *  183      Horizontal dagger (?)
+ *  184-186  Symbols {}\
+ *  187      Cents symbol
+ *  188      Centered dot, or bullet
+ *  189-255  Reserved
+ * </pre></code>
+ *
+ */
+public class PORCharset
+{
+
+    /**
+     * Translation table for the character set defined in a portable file.
+     * The table has the following format:<br>
+     * index {@code (k*2+0)} holds the symbol number, and <br>
+     * index {@code (k*2+1)} holds the character meant by the symbol number.<br><p>
+     * 
+     * When reading a portable file, the translation can be done as follows:<br>
+     * 1) Read symbol x from the portable file.<br>
+     * 2) Find the index for the symbol x in the character set defined in the
+     *    portable file.<br>
+     * 3) Find the corresponding index number from the translation table.<br>
+     * 4) Translate the input symbol x to the character found from the translation
+     *    table having the same index number.<br><p>
+     * 
+     * Writing a portable file can be done in the same manner.<p>
+     *
+     */
+    public static final int[] TRANS = {
+        // 0-60    Control characters. Not important enough to describe in full here. 
+        // 61-63   Reserved. 
+        
+        // 64-73   Digits `0' through `9'. 
+        64,     '0',
+        65,     '1',
+        66,     '2',
+        67,     '3',
+        68,     '4',
+        69,     '5',
+        70,     '6',
+        71,     '7',
+        72,     '8',
+        73,     '9',
+        
+        // 74-99   Capital letters `A' through `Z'. 
+        74,     'A',
+        75,     'B',
+        76,     'C',
+        77,     'D',
+        78,     'E',
+        79,     'F',
+        80,     'G',
+        81,     'H',
+        82,     'I',
+        83,     'J',
+        84,     'K',
+        85,     'L',
+        86,     'M',
+        87,     'N',
+        88,     'O',
+        89,     'P',
+        90,     'Q',
+        91,     'R',
+        92,     'S',
+        93,     'T',
+        94,     'U',
+        95,     'V',
+        96,     'W',
+        97,     'X',
+        98,     'Y',
+        99,     'Z',
+        
+        // 100-125 Lowercase letters `a' through `z'. 
+        100,    'a',
+        101,    'b',
+        102,    'c',
+        103,    'd',
+        104,    'e',
+        105,    'f',
+        106,    'g',
+        107,    'h',
+        108,    'i',
+        109,    'j',
+        110,    'k',
+        111,    'l',
+        112,    'm',
+        113,    'n',
+        114,    'o',
+        115,    'p',
+        116,    'q',
+        117,    'r',
+        118,    's',
+        119,    't',
+        120,    'u',
+        121,    'v',
+        122,    'w',
+        123,    'x',
+        124,    'y',
+        125,    'z',
+        
+        // 126     Space. 
+        126,    ' ',
+        
+        // 127-130 Symbols .<(+ 
+        127,    '.',
+        128,    '<',
+        129,    '(',
+        130,    '+',
+        
+        // 131     Solid vertical pipe. 
+        131,    -1,
+        
+        // 132-142 Symbols &[]!$*);^-/ 
+        132,    '&',
+        133,    '[',
+        134,    ']',
+        135,    '!',
+        136,    '$',
+        137,    '*',
+        138,    ')',
+        139,    ';',
+        140,    '^',
+        141,    '-',
+        142,    '/',
+        
+        // 143     Broken vertical pipe. 
+        // 144-150 Symbols ,%_>?`: 
+        // 151     British pound symbol. 
+        // 152-155 Symbols @'=". 
+        143,    '|', // ???
+        144,    ',',
+        145,    '%',
+        146,    '_',
+        147,    '>',
+        148,    '?',
+        149,    '`',
+        150,    ':',
+        151,    '#', // 0x23
+        152,    '@',
+        153,    '\'',
+        154,    '=',
+        155,    '\"',
+        
+        // 156     Less than or equal symbol. 
+        // 157     Empty box. 
+        // 158     Plus or minus. 
+        // 159     Filled box. 
+        // 160     Degree symbol. 
+        // 161     Dagger. 
+        // 162     Symbol `~'. 
+        // 163     En dash. 
+        // 164     Lower left corner box draw. 
+        // 165     Upper left corner box draw. 
+        // 166     Greater than or equal symbol. 
+        156,    -1,
+        157,    -1,
+        158,    -1,
+        159,    -1,
+        160,    -1,
+        161,    -1,
+        162,    '~', // 0x7E
+        163,    -1,
+        164,    -1,
+        165,    -1,
+        166,    -1,
+
+        // 167-176 Superscript `0' through `9'. 
+        167,    -1,
+        168,    -1,
+        169,    -1,
+        170,    -1,
+        171,    -1,
+        172,    -1,
+        173,    -1,
+        174,    -1,
+        175,    -1,
+        176,    -1,
+        
+        // 177     Lower right corner box draw. 
+        // 178     Upper right corner box draw. 
+        // 179     Not equal symbol. 
+        // 180     Em dash. 
+        // 181     Superscript `('. 
+        // 182     Superscript `)'. 
+        // 183     Horizontal dagger (?). 
+        177,    -1,
+        178,    -1,
+        179,    -1,
+        180,    -1,
+        181,    -1,
+        182,    -1,
+        183,    -1,
+        
+        // 184-186 Symbols `{}\'. 
+        184,    '{',
+        185,    '}',
+        186,    '\\',
+        
+        // 187     Cents symbol. 
+        // 188     Centered dot, or bullet. 
+        187,    -1,
+        188,    -1,
+        
+        // 189-255 Reserved. 
+    }; // TABLE
+    
+    // Some important constants
+    //==========================
+    
+    /** Full stop ('.'), symbol 127. */
+    public static final int FULL_STOP                            = 127;
+    
+    /** Plus sign ('+'), symbol 130. */
+    public static final int PLUS_SIGN                            = 130;
+    
+    /** Minus sign ('-'), symbol 141. */
+    public static final int MINUS_SIGN                           = 141;
+    
+    /** Slash ('/'), symbol 142. */
+    public static final int SLASH                                = 142;
+    
+    /** Digit zero ('0'), symbol 64. */
+    public static final int DIGIT_0                              = 64;
+    
+    /** Uppercase letter 'A', symbol 74. */
+    public static final int LETTER_UPPERCASE_A                   = 74;
+    
+    /** Uppercase letter 'Z', symbol 99. */
+    public static final int LETTER_UPPERCASE_Z                   = 99;
+
+    // CONSTRUCTORS
+    //==============
+    
+    /** Constructor intentionally disabled. */
+    private PORCharset() {
+    } // ctor
+
+    /*
+    public static final int[] getFullTable() {
+        if (FULL_TABLE == null) {
+            // Construct it
+            FULL_TABLE = new int[256];
+            for (int i = 0; i < FULL_TABLE.length; i++) {
+                int c = getIndex(i);
+                if (c == -1) {
+                    c = '0';
+                }
+                FULL_TABLE[i] = c;
+            } // for
+        } // if
+        return FULL_TABLE;
+    }
+    */
+    
+    /**
+     * Dumb search; fix with bisect
+     */
+    public static int getIndex(int index) {
+        int len = TRANS.length / 2;
+        for (int i = 0; i < len; i++) {
+            if (TRANS[i*2] == index) {
+                return TRANS[(i*2)+1];
+            }
+        } // for: each entry
+        return -1;
+    } // getIndex()
+    
+    
+} // class PORCharset
+
+
---
+++ src/java/spssio/por/PORSplashString.java
@@ -0,0 +1,123 @@
+//*******************************{begin:header}******************************//
+//                 spssio - http://code.google.com/p/spssio/                 //
+//***************************************************************************//
+//
+//      Java classes for reading and writing 
+//      SPSS/PSPP Portable and System files
+//
+//      Copyright (C) 2013 Jani Hautamaki <jani.hautamaki@hotmail.com>
+//
+//      Licensed under the terms of GNU General Public License v3.
+//
+//      You should have received a copy of the GNU General Public License v3
+//      along with this program as the file LICENSE.txt; if not, please see
+//      http://www.gnu.org/licenses/gpl-3.0.html
+//
+//********************************{end:header}*******************************//
+
+package spssio.por;
+
+/**
+ *
+ * The splash string "ASCII SPSS PORT FILE" in different character sets.<p>
+ *
+ * According to PSPP's documentation 
+ * <a href="http://www.gnu.org/software/pspp/pspp-dev/html_node/Portable-File-Header.html#Portable-File-Header">
+ *    A.3 Portable File Header
+ * </a>:<p>
+ * 
+ * The file begins with 200-byte header, which is divided into 5 sections
+ * each having 40 bytes and representing the same string in a different character
+ * character set encoding.<p>
+ *
+ * The splash string has the format "{@code <charset> SPSS PORT FILE}", where
+ * {@code <charset>} is the name of the character set used in the file, eg. ASCII
+ * or EBCDIC. Each 40-byte section is padded on the right with spaces in its 
+ * respective character set.<p>
+ * 
+ * Citing the PSPP's documentation: "It appears that these strings exist only to 
+ * inform those who might view the file on a screen, and that they are not parsed
+ * by SPSS products. Thus, they can be safely ignored."<p>
+ *
+ * According to the same documentation, the strings are suppoesd to be in 
+ * the following characters sets, in the specified order:
+ * <ol>
+ *    <li> EBCDIC, 
+ *    <li> 7-bit ASCII, 
+ *    <li> CDC 6-bit ASCII,
+ *    <li> 6-bit ASCII, 
+ *    <li> Honeywell 6-bit ASCII. 
+ * </ol><p>
+ *
+ * <b>TODO</b>: The splash string in other character sets should be calculated
+ * instead of using constants.<p>
+ *
+ */
+public class PORSplashString
+{
+    
+    /**
+     * 1st section:
+     * <code>"?????@????@????@????@@@@@@@@@@@@@@@@"</code> (EBCDIC)
+     */
+    public static final int[] EBCDIC = { 
+        0xc1, 0xe2, 0xc3, 0xc9, 0xc9, 0x40, 0xe2, 0xd7, 0xe2, 0xe2, 
+        0x40, 0xd7, 0xd6, 0xd9, 0xe3, 0x40, 0xc6, 0xc9, 0xd3, 0xc5, 
+        0x40, 0x40, 0x40, 0x40, 0x40, 0x40, 0x40, 0x40, 0x40, 0x40, 
+        0x40, 0x40, 0x40, 0x40, 0x40, 0x40, 0x40, 0x40, 0x40, 0x40 
+    };
+    
+    /**
+     * 2nd section:
+     * <code>"ASCII SPSS PORT FILE&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"</code> (7-bit ASCII)
+     */
+    public static final int[] ASCII_7BIT = {
+        0x41, 0x53, 0x43, 0x49, 0x49, 0x20, 0x53, 0x50, 0x53, 0x53, 
+        0x20, 0x50, 0x4f, 0x52, 0x54, 0x20, 0x46, 0x49, 0x4c, 0x45,
+        0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 
+        0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20
+    }; 
+    
+    /**
+     * 3rd section:
+     * <code>"00000-0000-0000-0000--------------------"</code> (CDC 6-bit ASCII)
+     */
+    public static final int[] CDC_ASCII_6BIT = {
+        0x30, 0x30, 0x30, 0x30, 0x30, 0x2d, 0x30, 0x30, 0x30, 0x30, 
+        0x2d, 0x30, 0x30, 0x30, 0x30, 0x2d, 0x30, 0x30, 0x30, 0x30, 
+        0x2d, 0x2d, 0x2d, 0x2d, 0x2d, 0x2d, 0x2d, 0x2d, 0x2d, 0x2d, 
+        0x2d, 0x2d, 0x2d, 0x2d, 0x2d, 0x2d, 0x2d, 0x2d, 0x2d, 0x2d 
+    }; 
+    
+    /**
+     * 4th section:
+     * <code>"!3#))0303300/240&),%000000000000000000000"</code> (6-bit ASCII)
+     */
+    public static final int[] ASCII_6BIT = {
+        0x21, 0x33, 0x23, 0x29, 0x29, 0x30, 0x33, 0x30, 0x33, 0x33, 
+        0x30, 0x30, 0x2f, 0x32, 0x34, 0x30, 0x26, 0x29, 0x2c, 0x25, 
+        0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 
+        0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30 
+    }; 
+    
+    /**
+     * 5th section:
+     * <code>"20200002'220'&)3000#000000000000000000000"</code> (Honeywell 6-bit ASCII)
+     */
+    public static final int[] HONEYWELL_ASCII_6BIT = {
+        0x30, 0x32, 0x30, 0x30, 0x30, 0x30, 0x32, 0x27, 0x32, 0x32, 
+        0x30, 0x27, 0x26, 0x29, 0x33, 0x30, 0x30, 0x30, 0x23, 0x30, 
+        0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30,
+        0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30 
+    }; 
+    
+    // CONSTRUCTORS
+    //==============
+    
+    /**
+     * Constructor intentionally disabled.
+     */
+    private PORSplashString() {
+    } // ctor
+    
+} // class PORSplash
