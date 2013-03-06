ac342e0adc4409a86e02b8575b4439274c6ae411
Jani Hautamaki <jani.hautamaki@hotmail.com>
2013-03-06 22:18:48 +0200

    [MOD] Fixed parse_value_labels(). The parsed objects weren't added to the PORFile. Added new functionality: dumps the PORFile contents after parsing. This is for test purposes only. In the end it will be a separate inspection application.

--- src/java/spssio/por/input/PORReader.java
+++ src/java/spssio/por/input/PORReader.java
@@ -25,6 +25,9 @@ import java.io.BufferedInputStream;
 import java.io.InputStream;
 import java.io.IOException;
 import java.io.FileNotFoundException;
+//  for iterating value labels (TODO: remove after initial dev)
+import java.util.Map;
+
 
 // spssio portable
 import spssio.por.PORFile;
@@ -33,6 +36,7 @@ import spssio.por.PORValue;
 import spssio.por.PORVariable;
 import spssio.por.PORMissingValue;
 import spssio.por.PORValueLabels;
+import spssio.por.PORHeader;
 
 // spssio common
 import spssio.common.SPSSFormat;
@@ -40,6 +44,7 @@ import spssio.common.SPSSFormat;
 // spssio utils
 import spssio.util.NumberSystem;
 import spssio.util.NumberParser;
+
 
 /*
  *
@@ -560,6 +565,9 @@ public class PORReader
             label = parse_string();
             valuelabels.mappings.put(value, label);
         } // for: value-label list
+        
+        // Add to PORFile object
+        por.labels.add(valuelabels);
     } // parse_value_labels()
                 
     protected void parse_document_records() {
@@ -923,6 +931,118 @@ public class PORReader
     }
     */
     
+    public static void printPortable(PORFile por) {
+        PORHeader header = por.header;
+        System.out.printf("Portable file contents\n");
+        System.out.printf("  Header:\n");
+        System.out.printf("     Signature:          \"%s\"\n", header.signature);
+        System.out.printf("     Version:            \'%c\'\n", header.version);
+        System.out.printf("     Creation date:      \"%s\"\n", header.date);
+        System.out.printf("     Creation time:      \"%s\"\n", header.time);
+        
+        if (header.software != null) {
+        System.out.printf("     Software:           \"%s\"\n", header.software);
+        } else {
+        System.out.printf("     Software:           <unset>\n");
+        } // if-else
+        
+        if (header.author != null) {
+        System.out.printf("     Author:             \"%s\"\n", header.author);
+        } else {
+        System.out.printf("     Author:             <unset>\n");
+        } // if-else
+
+        if (header.title != null) {
+        System.out.printf("     Title:              \"%s\"\n", header.title);
+        } else {
+        System.out.printf("     Title:              <unset>\n");
+        } // if-else
+
+        System.out.printf("     # of variables:     %d\n", header.nvariables);
+        
+        System.out.printf("     Precision:          %d base-30 digits\n", header.precision);
+        
+        if (header.weight_var_name != null) {
+        System.out.printf("     Weight variable:    \"%s\"\n", header.weight_var_name);
+        } else {
+        System.out.printf("     Weight variable:    <unset>\n");
+        } // if-else
+        
+        int size = por.variables.size();
+        System.out.printf("  Variables (%d):\n", size);
+        
+        for (int i = 0; i < size; i++) {
+        PORVariable cvar = por.variables.elementAt(i);
+        System.out.printf("     Name:               \"%s\"\n", cvar.name);
+        System.out.printf("     Width:              %d\n", cvar.width);
+            
+        if (cvar.label != null) {
+        System.out.printf("     Label:              \"%s\"\n", cvar.label);
+        } else {
+        System.out.printf("     Label:              <unsert>\n");
+        } // if-else
+        
+        System.out.printf("     Print fmt:          %d / %d / %d\n",
+            cvar.printfmt.type, cvar.printfmt.width, cvar.printfmt.decimals);
+        System.out.printf("     Wrint fmt:          %d / %d / %d\n",
+            cvar.writefmt.type, cvar.writefmt.width, cvar.writefmt.decimals);
+        
+        System.out.printf("     Missing values (%d)\n", cvar.missvalues.size());
+        for (int j = 0; j < cvar.missvalues.size(); j++) {
+            PORMissingValue miss = cvar.missvalues.elementAt(j);
+            String s = null;
+            switch(miss.type) {
+                case PORMissingValue.TYPE_DISCRETE_VALUE:
+                    s = String.format(
+                        "discrete: %s", 
+                        miss.values[0].value);
+                    break;
+                case PORMissingValue.TYPE_RANGE_OPEN_LO:
+                    s = String.format(
+                        "range:    --%s",
+                        miss.values[0].value);
+                    break;
+                case PORMissingValue.TYPE_RANGE_OPEN_HI:
+                    s = String.format(
+                        "range:    %s--",
+                        miss.values[0].value);
+                    break;
+                case PORMissingValue.TYPE_RANGE_CLOSED:
+                    s = String.format(
+                        "range:    %s--%s",
+                        miss.values[0].value,
+                        miss.values[1].value);
+                    break;
+                
+                default:
+                    s = "????? error";
+                    break;
+                
+            } // switch
+            System.out.printf("         %s\n", s);
+        } // for: missing values
+        
+        } // for: variables
+        
+        size = por.labels.size();
+        
+        System.out.printf("  Value labels lists (%d):\n", size);
+        for (int i = 0; i < size; i++) {
+            PORValueLabels labels = por.labels.elementAt(i);
+            
+        System.out.printf("     Variables:   %d\n", labels.vars.size());
+        for (int j = 0; j < labels.vars.size(); j++) {
+        System.out.printf("         \"%s\"\n", labels.vars.elementAt(j).name);
+        } // for: variables
+        System.out.printf("     Pairs:       %d\n", labels.mappings.size());
+        for (Map.Entry<PORValue, String> entry : labels.mappings.entrySet()) {
+        System.out.printf("          \"%s\" : \"%s\"\n",
+            entry.getKey().value, entry.getValue());
+        } // for: each mapping
+        
+        } // for: value labels lists
+    }
+    
     
     
     public static void main(String[] args) {
@@ -931,7 +1051,10 @@ public class PORReader
             
             for (int i = 0; i < args.length; i++) {
                 System.out.printf("%s\n", args[i]);
-                preader.parse(args[i]);
+                PORFile por = null;
+                por = preader.parse(args[i]);
+                
+                printPortable(por);
             }
         } catch(Exception ex) {
         } // try-catch
