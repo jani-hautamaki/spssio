27d01f484741a59180ef341cc2dd40448cd95a43
Jani Hautamaki <jani.hautamaki@hotmail.com>
2016-07-30 16:39:52 +0300

    Java header file updated, and the changes were propagated to all files. Modifications: URL was changed from google code to github, and years 2015-2016 were included in the copyright notice.

--- testrun.bat
+++ testrun.bat
@@ -1,20 +1,20 @@
 @echo off
-rem *******************************{begin:header}******************************
-rem                  spssio - http://code.google.com/p/spssio/
-rem ***************************************************************************
+rem *****************************{begin:header}********************************
+rem            spssio - https://github.com/jani-hautamaki/spssio
+rem *****************************{begin:header}********************************
 rem
-rem       Java classes for reading and writing
-rem       SPSS/PSPP Portable and System files
+rem     Java classes for reading and writing
+rem     SPSS/PSPP Portable and System files
 rem
-rem       Copyright (C) 2013-2014 Jani Hautamaki <jani.hautamaki@hotmail.com>
+rem     Copyright (C) 2013-2016 Jani Hautamaki <jani.hautamaki@hotmail.com>
 rem
-rem       Licensed under the terms of GNU General Public License v3.
+rem     Licensed under the terms of GNU General Public License v3.
 rem
-rem       You should have received a copy of the GNU General Public License v3
-rem       along with this program as the file LICENSE.txt; if not, please see
-rem       http://www.gnu.org/licenses/gpl-3.0.html
+rem     You should have received a copy of the GNU General Public License v3
+rem     along with this program as the file LICENSE.txt; if not, please see
+rem     http://www.gnu.org/licenses/gpl-3.0.html
 rem
-rem ********************************{end:header}*******************************
+rem ******************************{end:header}*********************************
 
 rem Call Java VM
 java -cp build; %*
